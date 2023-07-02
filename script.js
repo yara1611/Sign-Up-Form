7521a32a606fbe156bbae0ad05d695f77fee6554
yara <76455290+yara1611@users.noreply.github.com>
2023-07-02 04:46:42 +0300

    validation

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 pass = document.querySelector("input[name='pass']")
 conpass = document.querySelector("input[name='conpass']")
+mess=document.querySelector('#messageCPass')
 var check = function() {
     if (pass.value ===
       conpass.value) {
       pass.style.border='1px solid green'
       conpass.style.border='1px solid green'
+      mess.textContent=" "
       
     } else {
       pass.style.border='1px solid red'
       conpass.style.border='1px solid red'
-      
+      mess.textContent="Not matching"
     }
   }
